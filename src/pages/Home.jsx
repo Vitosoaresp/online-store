17a5fd81bfd20ecf3cb8ae9e9682dc8c36f74cdf
Leonan Costa <leonanfecosta@gmail.com>
2022-04-26 17:26:38 -0300

    resolve erro de lint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Categories from '../components/Categories';
 import { Link } from 'react-router-dom';
+import Categories from '../components/Categories';
 
 class Home extends React.Component {
   constructor() {
