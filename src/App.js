c5527014ac481bd957c25060b389c04b42571dc3
Leonan Costa <leonanfecosta@gmail.com>
2022-04-26 15:20:17 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as api from './services/api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  async componentDidMount() {
+    const categories = await api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola');
+    console.log(categories);
+    
+  }
+
+  render () {
+    return <h1>App</h1>
+  }
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch ('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await response.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const response = await fetch (`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const products = await response.json();
+  return products;
 }
