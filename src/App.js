409218ee967350936703cf475763e9e20183a09d
Leonan Costa <leonanfecosta@gmail.com>
2022-04-26 15:32:41 -0300

    corrige o lint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api'
 
-class App extends React.Component{
-  render () {
-    return <h1>App</h1>
+class App extends React.Component {
+  render() {
+    return <h1>App</h1>;
   }
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  const response = await fetch ('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const categories = await response.json();
   return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await fetch (`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const products = await response.json();
   return products;
 }
