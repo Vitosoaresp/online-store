ccc35763158f9609540f4a6b98d4024dff11db78
Leonan Costa <leonanfecosta@gmail.com>
2022-04-26 15:24:20 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './App.css';
 import * as api from './services/api'
 
 class App extends React.Component{
-  async componentDidMount() {
-    const categories = await api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola');
-    console.log(categories);
-    
-  }
-
   render () {
     return <h1>App</h1>
   }
