c4cd58de7d78415a10b930fbce8b1074cfd5b039
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-13 16:02:32 -0300

    muda home.jsx para componente funcional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,89 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Product from '../components/Product';
 import Categories from '../components/Categories';
 import styles from '../modules/Home.module.css';
 
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      search: '',
-      searchCategorie: '',
-    };
-  }
+function Home({
+  searchProduct, productList, loading, searchProductByCategory, handleClick }) {
+  const [search, setSearch] = useState('');
 
-  handleChange = ({ target }) => {
-    const { value } = target;
-    this.setState({ search: value });
+  const searchClean = () => {
+    searchProduct(search, '');
+    setSearch('');
   };
 
-  searchClean = () => {
-    const { search, searchCategorie } = this.state;
-    const { searchProduct } = this.props;
-    searchProduct(search, searchCategorie);
-    this.setState({ search: '' });
-  };
+  const showProducts = () => productList.map((item) => (
+    <Product
+      productName={ item.title }
+      productImage={ item.thumbnail }
+      productPrice={ item.price }
+      handleClick={ handleClick }
+      productId={ item.id }
+      freeShipping={ item.shipping.free_shipping }
+      key={ item.id }
+    />
+  ));
 
-  showProducts = () => {
-    const { handleClick, productList } = this.props;
-    return productList.map((item) => (
-
-      <Product
-        productName={ item.title }
-        productImage={ item.thumbnail }
-        productPrice={ item.price }
-        handleClick={ handleClick }
-        productId={ item.id }
-        freeShipping={ item.shipping.free_shipping }
-        key={ item.id }
-      />
-    ));
-  };
-
-  render() {
-    const { search } = this.state;
-    const { loading, searchProductByCategory, productList } = this.props;
-    return (
-      <div className={ styles.container }>
-        <section className={ styles.inputContainer }>
-          <input
-            data-testid="query-input"
-            name="search"
-            type="text"
-            value={ search }
-            onChange={ this.handleChange }
-            placeholder="Digite algum termo de pesquisa ou escolha uma categoria..."
+  return (
+    <div className={ styles.container }>
+      <section className={ styles.inputContainer }>
+        <input
+          data-testid="query-input"
+          name="search"
+          type="text"
+          value={ search }
+          onChange={ ({ target }) => setSearch(target.value) }
+          placeholder="Digite algum termo de pesquisa ou escolha uma categoria..."
+        />
+        <button
+          onClick={ searchClean }
+          type="button"
+          data-testid="query-button"
+        >
+          Buscar
+        </button>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </section>
+      <section className={ styles.main }>
+        <div className={ styles.categoriesContainer }>
+          <Categories
+            searchProductByCategory={ searchProductByCategory }
           />
-          <button
-            onClick={ this.searchClean }
-            type="button"
-            data-testid="query-button"
-          >
-            Buscar
-          </button>
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
-        </section>
-        <section className={ styles.main }>
-          <div className={ styles.categoriesContainer }>
-            <Categories
-              searchProductByCategory={ searchProductByCategory }
-            />
-          </div>
-          <div className={ styles.productsContainer }>
-            {loading && productList.length === 0 && (
-              <p className={ styles.notFound }>Nenhum produto foi encontrado</p>
-            )}
+        </div>
+        <div className={ styles.productsContainer }>
+          {loading && productList.length === 0 && (
+            <p className={ styles.notFound }>Nenhum produto foi encontrado</p>
+          )}
 
-            {this.showProducts()}
-          </div>
-
-        </section>
-      </div>
-    );
-  }
+          {showProducts()}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Home.propTypes = {
