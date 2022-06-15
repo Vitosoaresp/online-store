63b0545260c3be8d189e3fef9c6aeea92d68d658
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-14 21:45:12 -0300

    adiciona ordenação para preços e mais vendidos

--- src/context/OnlineStoreProvider.js
+++ src/context/OnlineStoreProvider.js
@@ -17,6 +17,7 @@ function OnlineStoreProvider({ children }) {
   const [isFetching, setIsFetching] = useState(false);
   const [hiddenMenu, setHiddenMenu] = useState(true);
   const [noProductsFound, setNoProductsFound] = useState(false);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -60,15 +61,11 @@ function OnlineStoreProvider({ children }) {
       ...product,
       quantity: 1,
     };
-    const incrementedItemCart = cartList.some((item) => item.id === itemId);
-    if (!incrementedItemCart) {
-      setCartList([...cartList, { ...newProduct }]);
-      return addProductLocalStorage(cartList);
-    }
+    const checkIncludesListCart = cartList.some((item) => item.id === itemId);
+    if (!checkIncludesListCart) return setCartList([...cartList, { ...newProduct }]);
     const productSelect = cartList.find((item) => item.id === itemId);
     productSelect.quantity += 1;
     setCartList([...cartList]);
-    addProductLocalStorage(cartList);
     localStorage.setItem('lengthCart', JSON.stringify(cartLength));
   }, [cartList, cartLength, productList]);
 
@@ -80,12 +77,11 @@ function OnlineStoreProvider({ children }) {
     };
     addProductLocalStorage(cartList);
     changeQuantityCart();
-  }, [cartList]);
+  }, [cartLength, cartList]);
 
   const checkProducts = () => {
     const products = cartList.filter((product) => product.quantity > 0);
     setCartList(products);
-    addProductLocalStorage(products);
   };
 
   const decrementQuantity = ({ target }) => {
@@ -94,7 +90,6 @@ function OnlineStoreProvider({ children }) {
     productSelect.quantity -= 1;
     setCartList([...cartList]);
     checkProducts();
-    addProductLocalStorage(cartList);
   };
 
   return (
@@ -105,6 +100,7 @@ function OnlineStoreProvider({ children }) {
           decrementQuantity,
           cartList,
           productList,
+          setProductList,
           isFetching,
           categories,
           selectedCategory,
