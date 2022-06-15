93b5b499f93c174d23ae9a33d161759fce2202a1
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-14 23:33:52 -0300

    mudanças na pagina ProductDetails, tira avaliações e adicona mais informações sobre o produto

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { AiFillStar } from 'react-icons/ai';
 import { getProductsFromId } from '../services/api';
 import styles from '../modules/ProductDetails.module.css';
 import OnlineStoreContext from '../context/OnlineStoreContext';
@@ -8,58 +7,66 @@ import OnlineStoreContext from '../context/OnlineStoreContext';
 function ProductDetails({ match }) {
   const { handleClick } = useContext(OnlineStoreContext);
   const [detailsProduct, setDetailsProduct] = useState({
-    id: '',
-    name: '',
+    title: '',
     price: '',
     image: '',
-    email: '',
-    message: '',
-    rating: '',
+    soldQuantity: '',
+    condition: '',
   });
-  const [listRating, setListRating] = useState(
-    JSON.parse(localStorage.getItem('rating')) || [],
-  );
+  const [attributes, setAttributes] = useState({
+    marca: '',
+    modelo: '',
+    condicao: '',
+  });
 
   useEffect(() => {
     const getId = async () => {
       const { params: { id } } = match;
       const product = await getProductsFromId(id);
-      const { thumbnail, title, price } = product;
-      setDetailsProduct({ ...detailsProduct, id, name: title, price, image: thumbnail });
+      console.log(product);
+      const { pictures, sold_quantity: soldQuantity } = product;
+      setDetailsProduct({
+        soldQuantity, image: pictures[0].url, ...product,
+      });
     };
     getId();
   }, []);
 
-  const getRating = ({ target }) => {
-    const { name, value } = target;
-    setDetailsProduct({ ...detailsProduct, [name]: value });
-  };
-
-  const saveRating = () => {
-    const { email, message, rating } = detailsProduct;
-    const previousRating = {
-      email,
-      message,
-      rating,
-    };
-    setListRating([...listRating, previousRating]);
-    setDetailsProduct({ ...detailsProduct, email: '', message: '', rating: '' });
-  };
-
   useEffect(() => {
-    localStorage.setItem('rating', JSON.stringify(listRating));
-  }, [listRating]);
+    if (detailsProduct.attributes) {
+      const marca = detailsProduct.attributes
+        .find((attribute) => attribute.name === 'Marca');
+      const modelo = detailsProduct.attributes
+        .find((attribute) => attribute.name === 'Modelo');
+      const condicao = detailsProduct.attributes
+        .find((attribute) => attribute.name === 'Condição do item');
+      setAttributes({
+        marca: marca.value_name,
+        modelo: modelo.value_name,
+        condicao: condicao.value_name });
+    }
+  }, [detailsProduct]);
 
-  const { name, price, image, id, email, message, rating } = detailsProduct;
+  const { title, price, image, id, soldQuantity } = detailsProduct;
   return (
     <div className={ styles.container }>
       <div className={ styles.productContainer }>
         <div className={ styles.productImage }>
-          <img src={ image } alt={ name } />
+          <img src={ image } alt={ title } />
         </div>
         <div className={ styles.productInfo }>
-          <p data-testid="product-detail-name">{name}</p>
+          <div className={ styles.productStats }>
+            <span>{`${attributes.condicao}`}</span>
+            <hr />
+            <span>{`${soldQuantity} Vendas`}</span>
+          </div>
+          <p data-testid="product-detail-name">{title}</p>
           <p>{`R$${price}`}</p>
+          <div className={ styles.productDetails }>
+            <p>{`Marca: ${attributes.marca}`}</p>
+            <p>{`Modelo: ${attributes.modelo}`}</p>
+            { detailsProduct.warranty && <p>{detailsProduct.warranty}</p> }
+          </div>
           <button
             data-testid="product-detail-add-to-cart"
             type="button"
@@ -70,112 +77,6 @@ function ProductDetails({ match }) {
             Adicione ao carrinho
           </button>
         </div>
-      </div>
-      <div className={ styles.ratingContainer }>
-        <h2> Avaliações</h2>
-        <div className={ styles.emailAndRating }>
-          <label htmlFor="email">
-            <input
-              data-testid="product-detail-email"
-              type="email"
-              name="email"
-              value={ email }
-              onChange={ getRating }
-              placeholder="Email"
-              className={ styles.email }
-            />
-          </label>
-          <label htmlFor="rating" className={ styles.rating }>
-            <input
-              checked={ rating === '1' }
-              type="radio"
-              value="1"
-              id="rating"
-              name="rating"
-              onChange={ getRating }
-              data-testid="1-rating"
-            />
-            <AiFillStar />
-          </label>
-          <label htmlFor="rating2" className={ styles.rating }>
-            <input
-              checked={ rating === '2' }
-              type="radio"
-              value="2"
-              id="rating2"
-              name="rating"
-              onChange={ getRating }
-              data-testid="2-rating"
-            />
-            <AiFillStar />
-          </label>
-          <label htmlFor="rating3" className={ styles.rating }>
-            <input
-              checked={ rating === '3' }
-              type="radio"
-              value="3"
-              id="rating3"
-              name="rating"
-              onChange={ getRating }
-              data-testid="3-rating"
-            />
-            <AiFillStar />
-          </label>
-          <label htmlFor="rating4" className={ styles.rating }>
-            <input
-              checked={ rating === '4' }
-              type="radio"
-              value="4"
-              id="rating4"
-              name="rating"
-              onChange={ getRating }
-              data-testid="4-rating"
-            />
-            <AiFillStar />
-          </label>
-          <label htmlFor="rating5" className={ styles.rating }>
-            <input
-              checked={ rating === '5' }
-              type="radio"
-              value="5"
-              id="rating5"
-              name="rating"
-              onChange={ getRating }
-              data-testid="5-rating"
-            />
-            <AiFillStar />
-          </label>
-        </div>
-        <label htmlFor="message">
-          <textarea
-            data-testid="product-detail-evaluation"
-            name="message"
-            value={ message }
-            onChange={ getRating }
-            placeholder="Mensagem(opcional)"
-            rows="4"
-            cols="50"
-          />
-        </label>
-        <button
-          data-testid="submit-review-btn"
-          type="submit"
-          onClick={ saveRating }
-          disabled={ !email.length && !rating.length }
-        >
-          Avaliar
-        </button>
-      </div>
-      <div className={ styles.ratingText }>
-        <ul>
-          {listRating.map((item, index) => (
-            <li key={ index }>
-              <p>{item.email}</p>
-              <p>{item.message}</p>
-              <p>{item.rating}</p>
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   );
