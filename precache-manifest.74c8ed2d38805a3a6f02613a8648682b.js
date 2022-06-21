88d268d991c43180a724b82c9cddfff52ddcd37f
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-20 23:42:46 -0300

    Updates

---
+++ precache-manifest.74c8ed2d38805a3a6f02613a8648682b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48eac93476693e49c7a02f268042455a",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "3bb7d92b8b987ad4e7f4",
+    "url": "/online-store/static/css/main.cdb7dda0.chunk.css"
+  },
+  {
+    "revision": "45c4862de5197f68eaeb",
+    "url": "/online-store/static/js/2.8fecba1b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store/static/js/2.8fecba1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb7d92b8b987ad4e7f4",
+    "url": "/online-store/static/js/main.08d5dfeb.chunk.js"
+  },
+  {
+    "revision": "ff4896c2419cfc1775c2",
+    "url": "/online-store/static/js/runtime-main.a451c0e5.js"
+  }
+]);
